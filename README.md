fb76460acb0a4371ef0e85c353d56e6bb3dbbe5c
ze32 <>
2025-01-15 10:33:11 +0800

    add files

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 # 随缘更新
 免费节点都爬取自网络，请勿用于非法用途  
 
+### Base64订阅链接
+- `https://github.com/ze32/VMesslinks/raw/main/links/v2ray`  源
+- `https://raw.gitmirror.com/ze32/VMesslinks/main/links/v2ray`  CDN
+
 ### VMess订阅链接
-- `https://github.com/Memory2314/VMesslinks/raw/main/links/vmess`  源
-- `https://raw.staticdn.net/Memory2314/VMesslinks/main/links/vmess`  CDN
+- `https://github.com/ze32/VMesslinks/raw/main/links/clash`  源
+- `https://raw.gitmirror.com/ze32/VMesslinks/main/links/clash`  CDN
+
+## Star History
+[![Star History Chart](https://api.star-history.com/svg?repos=ze32/VMesslinks&type=Date)](https://star-history.com/#ze32/VMesslinks&Date)
--- utils/get_vmess.py
+++ utils/get_vmess.py
@@ -1,48 +1,66 @@
+import os
+from datetime import datetime
+
 import requests
-from Crypto.Cipher import AES
-import base64
 
-# 接口URL
-url = "https://www.m4twf.xyz:20000/api/evmess?&proto=v2&platform=android&googleplay=1&ver=3.0.5&deviceid=1bcec3395995cf19unknown&unicode=1bcec3395995cf19unknown&t=1717462751804&code=9GFZ2R&recomm_code=&f=2024-06-04&install=2024-06-04&token=amSTaWVnkZWOk2xscWlsb5mZbmRolGuRZ2mQl5Jrkmhnaw==&package=com.honeybee.network&area=5"
+# 获取今天的日期
+year = datetime.now().year
+month = datetime.now().strftime("%m")
+day = datetime.now().strftime("%d")
 
-# 解密密钥和IV（文本）
-key = b'ks9KUrbWJj46AftX'
-iv = b'ks9KUrbWJj46AftX'
+# 构建 URL，日期部分会自动替换为当前日期
+url = f"https://clashnode.cc/uploads/{year}/{month}/0-{year}{month}{day}"
 
-# 存储解密后的节点信息
-decrypted_nodes = set()
+headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "priority": "u=0, i",
+    "sec-ch-ua": '"Chromium";v="130", "Google Chrome";v="130", "Not A Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
+}
 
-# 获取并解密节点信息
-def fetch_and_decrypt():
-    response = requests.get(url)
-    if response.status_code == 200:
-        encrypted_data = response.text.strip()  # 获取返回的加密字符串
-        try:
-            # 将Base64编码的加密数据解码
-            encrypted_data_bytes = base64.b64decode(encrypted_data)
-            # 创建AES解密器
-            cipher = AES.new(key, AES.MODE_CBC, iv)
-            # 解密数据
-            decrypted_data = cipher.decrypt(encrypted_data_bytes)
-            # 返回解密后的数据
-            return decrypted_data.decode('utf-8', errors='ignore').rstrip('\x00')
-        except Exception as e:
-            print(f"解密失败：{e}")
-            return None
-    else:
-        print(f"请求失败，状态码: {response.status_code}")
-        return None
+v2ray_node = ''
+clash_node = ''
 
-# 重复获取并解密节点信息10次
-for _ in range(10):
-    node_info = fetch_and_decrypt()
-    if node_info:
-        decrypted_nodes.add(node_info)
+# 发送 HTTP 请求获取链接内容
+try:
+    v2ray_response = requests.get(url+'.txt', headers=headers)
+    clash_response = requests.get(url+'.yaml', headers=headers)
+    v2ray_response.raise_for_status()  # 检查请求是否成功
+    clash_response.raise_for_status()
 
-# 保存去重后的节点信息
-vmess = ""
-for node in decrypted_nodes:
-    print(node)
-    vmess += f"{node}\n"
-with open("./links/vmess", "w") as f:
-    f.write(vmess)
+    print(f"成功获取到 v2ray_node 内容 (日期: {year}-{month}-{day}):")
+    v2ray_node = v2ray_response.text
+    print(v2ray_node)
+    print('-------------------------------------------------------------------------------------')
+    print(f"成功获取到 clash_node 内容 (日期: {year}-{month}-{day}):")
+    clash_node = clash_response.text
+    print(clash_node)
+
+except requests.exceptions.RequestException as e:
+    print(f"请求失败: {e}")
+
+# 定义文件夹路径
+links_directory = "./links"
+
+# 创建目录，如果不存在
+os.makedirs(links_directory, exist_ok=True)
+
+# 写入 v2ray 文件
+with open(os.path.join(links_directory, "v2ray"), "w", encoding='utf-8') as f:
+    f.write(v2ray_node)
+
+# 写入 clash 文件
+with open(os.path.join(links_directory, "clash"), "w", encoding='utf-8') as f:
+    f.write(clash_node)
+
+print("节点信息已保存")
